a521b3f082bee9a9314f6f765b948eeac2b1a3c2
GaryFelix <2k19cse020@kiot.ac.in>
2024-02-16 19:12:58 +0530

    Version 0.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,50 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HeaderComponent,
+    title: "AGF | Home",
+    children:[
+      {
+        path:'',
+        redirectTo: '/home',
+        pathMatch: 'full'
+      },
+      {
+        path:'home',
+        component: HomeComponent
+      },
+      {
+        path: 'about',
+        title: "AGF | About Me",
+        component: AboutComponent
+      },
+      {
+        path: 'services',
+        title: "AGF | Services",
+        component: ServicesComponent
+      },
+      {
+        path: 'contact',
+        title: "AGF | Contact Me",
+        component: ContactComponent
+      }
+    ]
+  },
+  {
+    path: '**',
+    component: NotfoundComponent,
+    title: 'Learn Angular | Page Not Found'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ContactComponent } from './components/contact/contact.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    HeaderComponent,
+    NotfoundComponent,
+    ServicesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
